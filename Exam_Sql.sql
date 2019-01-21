5256d3604ea600d75b274871608e32d4be5b3528
Prasanna <prasanna@hexaware.com>
2019-01-21 14:19:53 +0530

    Exam

---
+++ Exam_Sql.sql
@@ -0,0 +1,15 @@
+1 Display the last occurrence of given char 
+in a string Ex: 'nikita' display 2nd occ
+of 'i'
+
+2 Take name as 'Sanjana Kolage' split into 
+firstname and lastname 
+
+3 In word 'misissipi' count no.of i 
+
+4 Emp table Name field display no.of words 
+
+5 Display all names whose name starts and 
+ends with same char from EMP table
+
+Ex :  karthik, suhaas 
