d2a59886d28da71a069ea20687fd04246ea31703
Prasanna <prasanna@hexaware.com>
2019-01-22 15:02:04 +0530

    EmpInsert

---
+++ Emp_Insert_Update.sql
@@ -0,0 +1,13 @@
+use sqlpractice
+
+
+insert into emp(name,dept,desig,Basic)
+ values('bhaskar','java',
+'programmer',58334),
+('pavan','git','expert',56211),
+('sanjana','sql','programmer',56222),
+('sahil','sql','expert',99999),
+('nikita','agile','master',98444),
+('megha','git','expert',81455),
+('priyanka','java','programmer',62112),
+('krishna','SAP','Programmer',66222)
