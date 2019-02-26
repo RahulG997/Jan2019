c36862e3186f4a1a924ce2e220617693934f3ace
prasanna-hexaware <prasanna-hexaware@github.com>
2019-02-26 14:57:40 +0530

    Changes

---
+++ SnapShotDemo/app/third/third.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute,Router } from '@angular/router';
+@Component({
+  selector: 'app-third',
+  templateUrl: './third.component.html',
+  styleUrls: ['./third.component.css']
+})
+export class ThirdComponent implements OnInit {
+  name : string;
+  constructor(private route : ActivatedRoute) { 
+    this.name=this.route.snapshot.params["name"];
+  }
+  ngOnInit() {
+  }
+}
