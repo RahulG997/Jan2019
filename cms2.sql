612bbec89f6ee921c3675326fd796c8bf86293bc
Prasanna <prasanna@hexaware.com>
2020-01-06 22:14:00 +0530

    ch

---
+++ cms2.sql
@@ -0,0 +1,11 @@
+ INSERT INTO orders(CUS_ID,VEN_ID,WAL_SOURCE,MEN_ID,ORD_DATE,ORD_QUANTITY,ORD_BILLAMOUNT,
+ORD_STATUS,ORD_COMMENTS)
+ VALUES (1,3,'PAYTM',3, '2020-01-05',2,1131,'PENDING','Is it possible')
+
+select * from Orders;
+
+select * from Orders;
+
+select * from vendor;
+
+select * from Orders
